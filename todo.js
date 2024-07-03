eb3e2f52cfa0be0f9fffd77251d4dbcd50f97aa7
Ficus <s.savchenko@tg.dunice.net>
2024-07-03 14:27:36 +0300

    arr

--- todo.js
+++ todo.js
@@ -1,10 +1,21 @@
 
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
+let a7 = []
 
 form.addEventListener('submit', function(event) {
 event.preventDefault();
 const taskText = input.value
 console.log(taskText);
 })
+function inputarr(){
+  let input = document.querySelector('#input')
+
+  a7.push(input.value)
+  console.log(a7)
+
+}
+
+document.querySelector('#input').onclick = inputarr
+
 
