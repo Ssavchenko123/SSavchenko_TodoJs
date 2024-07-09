641821eb4a80f269f1fbb85188882ed1bc584b5d
Ficus <s.savchenko@tg.dunice.net>
2024-07-09 17:42:55 +0300

    blur, allChecked, filter,

--- todo.js
+++ todo.js
@@ -1,22 +1,27 @@
 
 const ENTER = 'Enter';
 const ESC = 'Escape';
+const checkAllTask = document.querySelector('#check-box-up');
 const form = document.querySelector('#form');
 const inputMain = document.querySelector('#input');
 const buttonAdd = document.querySelector('#button');
 const list = document.querySelector('#todo-elements')
 const newTask = document.querySelector('#newText')
+const Bottom = document.querySelector('#bottom-buttons')
 let taskArray = []
+let filterName = "All"
 
 function addTask(event) {
   event.preventDefault();
+if(inputMain.value.trim() !=""){
   taskArray.push(
     {
       id: Date.now(),
       taskText: inputMain.value,
-      isComplited: false,
+      isCompleted: false,
     }
   )
+}
   inputMain.value = "";
   render()
 }
@@ -26,8 +31,8 @@ const render = () => {
   taskArray.forEach((task) => {
     renderElements += `
     <li class="task-block" id="${task.id}">
-      <input class="check-box" type="checkbox">
-      <input hidden class = "newText">
+      <input class="check-box" type="checkbox" ${task.isCompleted ? 'checked' : ''}>
+      <input hidden class = "newText" value = "${task.taskText}" required>
       <pre class="task-name">${task.taskText}</pre>
       <button class="delete" id>X</button>
     </li>
@@ -45,7 +50,7 @@ function handleClick(event) {
 
   if (event.target.className === 'check-box') {
     taskArray.forEach((task) => {
-      if (task.id == parentId) task.isComplited = event.target.checked;
+      if (task.id == parentId) task.isCompleted = event.target.checked;
     });
   }
   if (event.target.className === 'task-name') {
@@ -53,35 +58,56 @@ function handleClick(event) {
       event.target.hidden = true;
       event.target.previousElementSibling.hidden = false;
       event.target.previousElementSibling.focus();
-  
       event.target.previousElementSibling.innerText = event.target.innerText
     }
   }
-
+  
 }
 const blurTask = (event) => {
-  taskText.blur();
+  if(event.target.value.trim() !="" && event.target.className === 'input'){
+  taskArray.forEach(task => {
+    if(task.id == event.target.parentElement.id){
+      task.taskText = event.target.value
+    }
+  })
+}
   render()
 }
+
 const editTask = (event) => {
   if (event.code === ENTER) {
+    if(event.target.value.trim() !=""){
     const parentId = event.target.parentElement.id
-    console.log(event)
     taskArray.forEach(task => {
-      console.log(event)
       if (task.id === Number(parentId)) {
         task.taskText = event.target.value
       };
     });
+  }
     render()
   }
   else if (event.code === ESC) {
     render()
   }
 }
+const allChecked = (event) => {
+  taskArray.forEach(task => {
+    task.isCompleted = event.target.checked;
+  });
+  render()
+} 
+const filter = (event)=>{
+  if ( event.target.className === 'flex-element'){
+    console.log(event.target.id)
+    filterName = event.target.id
+    console.log(filterName)
+  
+  }
 
-
-list.addEventListener("blur", blurTask)
+}
+Bottom.addEventListener("click", filter)
+checkAllTask.addEventListener("click", allChecked);
+list.addEventListener("blur", blurTask, true);
 list.addEventListener("keydown", editTask);
 list.addEventListener('click', handleClick);
 buttonAdd.addEventListener("click", addTask)
