fff7900a5b17161d151f12ddc626e1411c851d62
Ficus <s.savchenko@tg.dunice.net>
2024-07-10 15:00:28 +0300

    blur, allChecked(correct), filter, counter, selector

--- todo.js
+++ todo.js
@@ -13,31 +13,35 @@ let filterName = "All"
 
 function addTask(event) {
   event.preventDefault();
-if(inputMain.value.trim() !=""){
-  taskArray.push(
-    {
-      id: Date.now(),
-      taskText: inputMain.value,
-      isCompleted: false,
-    }
-  )
-}
+  if (inputMain.value.trim() != "") {
+    taskArray.push(
+      {
+        id: Date.now(),
+        taskText: inputMain.value,
+        isCompleted: false,
+      }
+    )
+  }
   inputMain.value = "";
   render()
 }
 
 const render = () => {
+  counter()
+  checkAllTask.checked = taskArray.every(task => task.isCompleted) && taskArray.length;
+  const rend = filterArray()
   let renderElements = ""
-  taskArray.forEach((task) => {
+  rend.forEach((task) => {
     renderElements += `
     <li class="task-block" id="${task.id}">
       <input class="check-box" type="checkbox" ${task.isCompleted ? 'checked' : ''}>
-      <input hidden class = "newText" value = "${task.taskText}" required>
+      <input hidden class = "newText" value = "${task.taskText}" >
       <pre class="task-name">${task.taskText}</pre>
       <button class="delete" id>X</button>
     </li>
     `
   });
+
   list.innerHTML = renderElements;
 }
 
@@ -52,6 +56,7 @@ function handleClick(event) {
     taskArray.forEach((task) => {
       if (task.id == parentId) task.isCompleted = event.target.checked;
     });
+    render()
   }
   if (event.target.className === 'task-name') {
     if (event.detail == 2) {
@@ -61,29 +66,29 @@ function handleClick(event) {
       event.target.previousElementSibling.innerText = event.target.innerText
     }
   }
-  
+
 }
 const blurTask = (event) => {
-  if(event.target.value.trim() !="" && event.target.className === 'input'){
-  taskArray.forEach(task => {
-    if(task.id == event.target.parentElement.id){
-      task.taskText = event.target.value
-    }
-  })
-}
+  if (event.target.value.trim() != "" && event.target.className === 'newText') {
+    taskArray.forEach(task => {
+      if (task.id == event.target.parentElement.id) {
+        task.taskText = event.target.value
+      }
+    })
+  }
   render()
 }
 
 const editTask = (event) => {
   if (event.code === ENTER) {
-    if(event.target.value.trim() !=""){
-    const parentId = event.target.parentElement.id
-    taskArray.forEach(task => {
-      if (task.id === Number(parentId)) {
-        task.taskText = event.target.value
-      };
-    });
-  }
+    if (event.target.value.trim() != "") {
+      const parentId = event.target.parentElement.id
+      taskArray.forEach(task => {
+        if (task.id === Number(parentId)) {
+          task.taskText = event.target.value
+        };
+      });
+    }
     render()
   }
   else if (event.code === ESC) {
@@ -95,16 +100,42 @@ const allChecked = (event) => {
     task.isCompleted = event.target.checked;
   });
   render()
-} 
-const filter = (event)=>{
-  if ( event.target.className === 'flex-element'){
-    console.log(event.target.id)
-    filterName = event.target.id
-    console.log(filterName)
-  
-  }
-
 }
+const filter = (event) => {
+  if (event.target.className === 'flex-element') {
+    filterName = event.target.id
+    render()
+  }
+}
+const counter = () => {
+  const completedCount = taskArray.filter(task => task.isCompleted).length;
+  Bottom.children[2].innerText = `Completed ${completedCount}`
+  Bottom.children[1].innerText = `Active ${taskArray.length - completedCount}`
+  Bottom.children[0].innerText = `All ${taskArray.length}`
+}
+const filterArray = () => {
+  if (filterName === "All") {
+    return taskArray
+  }
+  else if (filterName === "Active") {
+    return taskArray.filter(task => !task.isCompleted)
+  }
+  else if (filterName === "Completed") {
+    return taskArray.filter(task => task.isCompleted)
+  }
+}  
+const selected = (event) =>{
+  if (event.target.className === 'flex-element') {
+    // Bottom.querySelectorAll(".bottom-buttons").forEach(task=>task.classList.remove('flex-element-changed'))
+    Bottom.querySelectorAll('.bottom-buttons').forEach(task=> console.log(task))
+    const a = Bottom.querySelectorAll('.flex-elements')
+    console.log(a);
+    event.target.classList.add("flex-element-changed")
+  }
+  render()
+}
+
+Bottom.addEventListener("click", selected)
 Bottom.addEventListener("click", filter)
 checkAllTask.addEventListener("click", allChecked);
 list.addEventListener("blur", blurTask, true);
