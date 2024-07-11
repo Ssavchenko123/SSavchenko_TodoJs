6d2331fadc09034c474d6adc903ba9cff6499e52
Ficus <s.savchenko@tg.dunice.net>
2024-07-11 12:34:17 +0300

    sliser+ pageSelector

--- todo.js
+++ todo.js
@@ -1,15 +1,21 @@
 
 const ENTER = 'Enter';
 const ESC = 'Escape';
+const TASK_PER_PAGE = 5;
 const checkAllTask = document.querySelector('#check-box-up');
 const form = document.querySelector('#form');
 const inputMain = document.querySelector('#input');
 const buttonAdd = document.querySelector('#button');
 const list = document.querySelector('#todo-elements')
-const newTask = document.querySelector('#newText')
 const Bottom = document.querySelector('#bottom-buttons')
+const buttons = Bottom.querySelectorAll('.flex-element')
+const newTask = document.querySelector('#newText')
+const pages = document.querySelector('.pages')
+//const deleteAll = document.querySelector('#delete-button')
 let taskArray = []
 let filterName = "All"
+const firstPage = 1;
+let totalPages = 1;
 
 function addTask(event) {
   event.preventDefault();
@@ -29,7 +35,11 @@ function addTask(event) {
 const render = () => {
   counter()
   checkAllTask.checked = taskArray.every(task => task.isCompleted) && taskArray.length;
-  const rend = filterArray()
+  const rend = slicePage()
+
+  if (taskArray.length <= 0) {
+    pages.innerHTML = '';
+  }
   let renderElements = ""
   rend.forEach((task) => {
     renderElements += `
@@ -37,12 +47,15 @@ const render = () => {
       <input class="check-box" type="checkbox" ${task.isCompleted ? 'checked' : ''}>
       <input hidden class = "newText" value = "${task.taskText}" >
       <pre class="task-name">${task.taskText}</pre>
-      <button class="delete" id>X</button>
+      <button class="delete">X</button>
     </li>
     `
   });
 
   list.innerHTML = renderElements;
+  renderPage()
+  renderPages()
+
 }
 
 function handleClick(event) {
@@ -58,6 +71,7 @@ function handleClick(event) {
     });
     render()
   }
+
   if (event.target.className === 'task-name') {
     if (event.detail == 2) {
       event.target.hidden = true;
@@ -68,6 +82,7 @@ function handleClick(event) {
   }
 
 }
+
 const blurTask = (event) => {
   if (event.target.value.trim() != "" && event.target.className === 'newText') {
     taskArray.forEach(task => {
@@ -106,12 +121,17 @@ const filter = (event) => {
     filterName = event.target.id
     render()
   }
+  if (event.target.className == 'flex-element') {
+    buttons.forEach(task => task.classList.remove('active'));
+    event.target.classList.add("active")
+    render()
+  }
 }
 const counter = () => {
   const completedCount = taskArray.filter(task => task.isCompleted).length;
-  Bottom.children[2].innerText = `Completed ${completedCount}`
-  Bottom.children[1].innerText = `Active ${taskArray.length - completedCount}`
-  Bottom.children[0].innerText = `All ${taskArray.length}`
+  Bottom.children[2].innerText = `Completed (${completedCount})`
+  Bottom.children[1].innerText = `Active (${taskArray.length - completedCount})`
+  Bottom.children[0].innerText = `All (${taskArray.length})`
 }
 const filterArray = () => {
   if (filterName === "All") {
@@ -123,20 +143,62 @@ const filterArray = () => {
   else if (filterName === "Completed") {
     return taskArray.filter(task => task.isCompleted)
   }
-}  
-const selected = (event) =>{
-  if (event.target.className === 'flex-element') {
-    // Bottom.querySelectorAll(".bottom-buttons").forEach(task=>task.classList.remove('flex-element-changed'))
-    Bottom.querySelectorAll('.bottom-buttons').forEach(task=> console.log(task))
-    const a = Bottom.querySelectorAll('.flex-elements')
-    console.log(a);
-    event.target.classList.add("flex-element-changed")
-  }
+}
+const deleteCompleted = () => {
+  taskArray = taskArray.filter(task => !task.isCompleted)
   render()
 }
+const renderPages = () => {
+  const filteredArr = filterArray();
+  totalPages = Math.ceil(filteredArr.length / TASK_PER_PAGE)
+  console.log(totalPages)
 
-Bottom.addEventListener("click", selected)
-Bottom.addEventListener("click", filter)
+}
+const renderPage = () => {
+  let createPages = `<button class="counter">1</button>`
+  //console.log(createPages)
+  for (let i = 2; i <= totalPages; i++) {
+    createPages += `<button class="counter">${i}</button>`;
+  }
+  pages.innerHTML = createPages;
+}
+const replace = () => {
+  return taskText.replace()
+}
+
+const slicePage = () => {
+  const filteredArray = filterArray()
+  const elementOnEnd = TASK_PER_PAGE * totalPages;
+  console.log(elementOnEnd)
+  const firstElement = elementOnEnd - TASK_PER_PAGE;
+  console.log(firstElement)
+  const pagination = filteredArray.slice(firstElement, elementOnEnd)
+  console.log(pagination)
+  return pagination;
+}
+// const selected = (event) =>{
+//   if (event.target.className =='flex-element') {
+//     buttons.forEach(task=>task.classList.remove('active'));
+//     event.target.classList.add("active")
+//     render()
+//   }
+// }
+
+//Bottom.querySelectorAll(".bottom-buttons").forEach(task=>{
+    //   if (task.id == event.target.id) {
+    //     console.log(task)
+    //   task.classList.remove('flex-element-changed')
+    // }
+    
+    // })
+    // 
+
+
+// const a = Bottom.querySelectorAll('.flex-element')
+    // console.log(a);
+// Bottom.addEventListener("click", selected);
+deleteAll.addEventListener("click", deleteCompleted);
+Bottom.addEventListener("click", filter);
 checkAllTask.addEventListener("click", allChecked);
 list.addEventListener("blur", blurTask, true);
 list.addEventListener("keydown", editTask);
